99b370a2f7ccfebedc4146f0f2b07f9b47d8d952
Lucas Muniz <lucasmunizfk@gmail.com>
2025-01-08 22:22:16 -0300

    WevSecurity sem adapter agora tá funcionando corretamente

--- src/main/java/proj/web_api/security/WebSecurityConfig.java
+++ src/main/java/proj/web_api/security/WebSecurityConfig.java
@@ -2,24 +2,45 @@ package proj.web_api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/").permitAll() // Permite acesso público à raiz
-                .requestMatchers(HttpMethod.POST, "/login").permitAll() // Permite acesso público à página de login
-                .requestMatchers("/managerAuth").hasRole("manager") // Apenas usuários com o papel "manager"
-                .requestMatchers("/usersAuth").hasAnyRole("user", "manager") // Usuários com os papéis "user" ou "manager"
-                .anyRequest().authenticated() // Todas as outras solicitações precisam de autenticação
-            );
-
+            .authorizeHttpRequests((authz) -> authz
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/managersAuth").hasRole("manager")
+                .requestMatchers("/usersAuth").hasAnyRole("user","manager")
+                .anyRequest().authenticated()
+            )
+            .formLogin(withDefaults());
         return http.build();
+    }
+
+        @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = org.springframework.security.core.userdetails.User.builder()
+            .username("user")
+            .password("{noop}user123")
+            .roles("user")
+            .build();
+
+        
+        UserDetails admin = org.springframework.security.core.userdetails.User.builder()
+            .username("admin")
+            .password("{noop}admin123") 
+            .roles("manager")
+            .build();
+
+        
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
